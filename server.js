973a0d5322c4fbe7feca506133e41c6551dd9169
rajranawatyadav-afk <rajranawatyadav@gmail.com>
2026-01-08 17:25:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,32 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const productRoutes = require("./routes/productRoutes");
+
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// ENV
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
-
 // MongoDB connection
 mongoose
-  .connect(MONGO_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
+
+// Routes
+app.use("/api/products", productRoutes);
 
 // Test route
 app.get("/", (req, res) => {
   res.send("Atrangi Backend is Live 🚀");
 });
 
-// Routes
-const productRoutes = require("./routes/productRoutes");
-app.use("/api/products", productRoutes);
-
-// Start server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
